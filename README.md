04e50113736ea721573f59ba21b0786a3cc3a948
PrudhviRaj5 <raj.prudhvi5@gmail.com>
2017-01-13 23:31:56 +0530

    adding setup, licence and readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# mongo-query-aggregator
+
+### Installation
+using pip:
+
+```sh
+$ pip install qgprofiler
+```
+
+using setuptools:
+
+```sh
+$ git clone https://github.com/quantumgraph/qgprofiler
+$ cd qgprofiler
+$ pip install qgprofiler
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='qgprofiler',
+    version='0.1.1',
+    description='Profiler with time deterministic profiling of functions',
+    url='https://github.com/quantumgraph/qgprofiler',
+    author='QuantumGraph',
+    author_email='contact@quantumgraph.com',
+    license='MIT',
+    packages=['qgprofiler'],
+    long_description=read('README.md'),
+    keywords = ['testing', 'logging', 'example'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: MIT License',
+    ],
+)
