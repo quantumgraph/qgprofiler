34338cc9dded996c816ab0540f5bc485a00ce5fa
PrudhviRaj5 <raj.prudhvi5@gmail.com>
2017-01-16 11:33:43 +0530

    version updated to 1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name='qgprofiler',
-    version='0.1.1',
+    version='0.1.2',
     description='Profiler with time deterministic profiling of functions',
     url='https://github.com/quantumgraph/qgprofiler',
     author='QuantumGraph',
